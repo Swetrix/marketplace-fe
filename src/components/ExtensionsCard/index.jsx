8e402665716d00f0057aeae298ea5919d691293a
kruzhambus <maks.mrug.ru@gmail.com>
2022-08-26 22:13:04 +0300

    fixed extensionsCard and added test data

--- src/components/ExtensionsCard/index.jsx
+++ src/components/ExtensionsCard/index.jsx
@@ -7,45 +7,56 @@ import { DownloadIcon } from '@heroicons/react/outline'
 
 const ExtensionsCard = ({
   name, stars, downloads, imagePath, price, companyLink, companyName,
-}) => (
-  <div className='group w-[210px] relative border-2 border-white rounded-sm p-3 bg-gray-100 dark:bg-gray-800 dark:border-gray-900  shadow-md'>
-    <div className='h-28 w-28 mx-auto aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none'>
-      <img src={imagePath} alt={companyName} className='w-full h-full object-center object-cover lg:w-full lg:h-full' />
-    </div>
-    <div className='mt-4'>
-      <h3 className='text-center text-lg font-semibold leading-5 text-gray-700 dark:text-gray-300'>
-        <a href='/'>
-          {name}
-        </a>
-      </h3>
-      <div className='flex items-center justify-between mt-2'>
-        <div className='flex flex-col'>
-          <p className='dark:text-gray-400 text-gray-500 text-sm leading-[10px]'>
-            {companyName}
-          </p>
-          <a href={companyLink} className='dark:text-indigo-400 cursor-pointer text-indigo-500 font-semibold border-0 text-sm'>
-            {_includes(companyLink, 'https://') ? _replace(companyLink, 'https://', '') : _replace(companyLink, 'http://', '')}
+}) => {
+  const subStr = (string, len) => {
+    if (string.length > len) {
+      return `${string.substring(0, len)}...`
+    }
+    return string
+  }
+
+  return (
+    <div className='group w-[210px] relative border-2 border-white rounded-lg p-3 bg-gray-100 dark:bg-gray-800 dark:border-gray-900'>
+      <div className='h-28 w-28 mx-auto aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none'>
+        <img src={imagePath} alt={companyName} className='w-full h-full object-center object-cover lg:w-full lg:h-full' />
+      </div>
+      <div className='mt-4'>
+        <h3 className='text-center text-lg font-semibold leading-5 text-gray-700 dark:text-gray-300'>
+          <a href='/'>
+            {name}
           </a>
+        </h3>
+        <div className='flex items-center justify-between mt-2'>
+          <div className='flex flex-col'>
+            <p className='dark:text-gray-400 text-gray-500 text-sm leading-[10px]'>
+              {subStr(name, 10)}
+            </p>
+            <a href={companyLink} className='dark:text-indigo-400 cursor-pointer text-indigo-500 font-semibold border-0 text-sm'>
+              {_includes(companyLink, 'https://')
+                ? subStr(_replace(companyLink, 'https://', ''), 10)
+                : subStr(_replace(companyLink, 'http://', ''), 10)}
+            </a>
+          </div>
+          <div className='flex items-center'>
+            <DownloadIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+            <p className='pl-1 text-gray-400'>
+              {downloads}
+              k
+            </p>
+          </div>
         </div>
-        <div className='flex items-center'>
-          <DownloadIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-          <p className='pl-1 text-gray-400'>
-            {downloads}
-            k
-          </p>
-        </div>
-      </div>
-      <div className='flex justify-between items-center mt-1'>
-        <StarsRaiting stars={stars} />
-        <div>
-          <p className='text-indigo-700 dark:text-indigo-500 font-semibold'>
-            {price === 0 ? 'Free' : `${price}$`}
-          </p>
-          <span className='sr-only'>pricing</span>
+        <div className='flex justify-between items-center mt-1'>
+          <StarsRaiting stars={stars} />
+          <div>
+            <p className='text-indigo-700 dark:text-indigo-500 font-semibold'>
+              {price === 0 ? 'Free' : `${price}$`}
+            </p>
+            <span className='sr-only'>pricing</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ExtensionsCard
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -4,28 +4,59 @@ import { SearchIcon } from '@heroicons/react/outline'
 import Dropdown from 'ui/Dropdown'
 import { sortBy, category } from 'redux/constants'
 import _values from 'lodash/values'
+import ExtensionsCard from 'components/ExtensionsCard'
+import _map from 'lodash/map'
 
-const testData = [
-  {
-    id: 1,
-    name: 'Extension 1',
-    description: 'This is the first extension',
-    category: 'category 1',
-    version: '1.0.0',
-    stars: 4,
-    downloads: 100,
-    createdAt: '2020-01-01',
-    updatedAt: '2020-01-01',
-    price: '$0.00',
-  },
-]
+const testData = [{
+  createdAt: '2022-08-26T01:17:18.876Z', name: 'Marcella Goyette', description: 'aspernatur velit ab', imagePath: 'http://loremflickr.com/640/480/transport', companyLink: 'http://parched-watercress.org', companyName: 'Cummings and Sons', stars: 60, downloads: 81, price: 18, id: '1',
+}, {
+  createdAt: '2022-08-26T07:24:27.069Z', name: 'Peter Kilback', description: 'omnis autem incidunt', imagePath: 'http://loremflickr.com/640/480/people', companyLink: 'https://ultimate-step-mother.biz', companyName: 'Schaefer LLC', stars: 61, downloads: 44, price: 88, id: '2',
+}, {
+  createdAt: '2022-08-26T03:55:25.567Z', name: 'Olive Mohr DVM', description: 'aut facilis soluta', imagePath: 'http://loremflickr.com/640/480/people', companyLink: 'https://corrupt-raisin.name', companyName: 'Paucek, Romaguera and Schowalter', stars: 68, downloads: 54, price: 32, id: '3',
+}, {
+  createdAt: '2022-08-26T01:32:37.589Z', name: 'Minnie Kemmer', description: 'culpa asperiores non', imagePath: 'http://loremflickr.com/640/480/sports', companyLink: 'http://shadowy-curio.com', companyName: 'Connelly and Sons', stars: 18, downloads: 94, price: 25, id: '4',
+}, {
+  createdAt: '2022-08-25T21:36:42.477Z', name: 'Mr. George Heller', description: 'et dolore cum', imagePath: 'http://loremflickr.com/640/480/transport', companyLink: 'https://crooked-faith.net', companyName: 'Kub and Sons', stars: 63, downloads: 18, price: 90, id: '5',
+}, {
+  createdAt: '2022-08-26T07:23:33.526Z', name: 'Natasha Lueilwitz', description: 'unde quis fugiat', imagePath: 'http://loremflickr.com/640/480/sports', companyLink: 'http://bossy-documentation.info', companyName: 'Zboncak, Rowe and Wiza', stars: 7, downloads: 24, price: 58, id: '6',
+}, {
+  createdAt: '2022-08-26T02:37:05.141Z', name: 'Leo Jacobs', description: 'maiores ratione assumenda', imagePath: 'http://loremflickr.com/640/480/food', companyLink: 'https://giant-princess.net', companyName: 'Witting LLC', stars: 57, downloads: 71, price: 42, id: '7',
+}, {
+  createdAt: '2022-08-26T12:39:48.376Z', name: 'Hugh Fadel', description: 'tempora voluptatem nemo', imagePath: 'http://loremflickr.com/640/480/sports', companyLink: 'http://putrid-apse.com', companyName: 'Flatley - Daniel', stars: 22, downloads: 68, price: 31, id: '8',
+}, {
+  createdAt: '2022-08-26T00:06:39.266Z', name: 'Albert Hudson DDS', description: 'dolore iure sint', imagePath: 'http://loremflickr.com/640/480/people', companyLink: 'https://worthless-ladle.org', companyName: 'Wisoky LLC', stars: 66, downloads: 89, price: 75, id: '9',
+}, {
+  createdAt: '2022-08-26T04:51:45.492Z', name: 'Perry Stehr', description: 'aut eligendi consequatur', imagePath: 'http://loremflickr.com/640/480/food', companyLink: 'http://plump-vaccine.org', companyName: 'Wolf LLC', stars: 93, downloads: 96, price: 79, id: '10',
+}, {
+  createdAt: '2022-08-25T21:37:33.768Z', name: 'Angelica Ritchie', description: 'rerum quos quae', imagePath: 'http://loremflickr.com/640/480/technics', companyLink: 'https://favorable-coonskin.org', companyName: 'Erdman, Parker and Aufderhar', stars: 70, downloads: 71, price: 72, id: '11',
+}, {
+  createdAt: '2022-08-25T19:51:56.933Z', name: 'Donnie Lakin', description: 'consectetur sit et', imagePath: 'http://loremflickr.com/640/480/city', companyLink: 'https://cylindrical-pomelo.info', companyName: 'Gorczany, Hoppe and Champlin', stars: 29, downloads: 74, price: 25, id: '12',
+}, {
+  createdAt: '2022-08-25T19:02:15.329Z', name: 'Clara Boyer', description: 'sint praesentium ratione', imagePath: 'http://loremflickr.com/640/480/transport', companyLink: 'https://mortified-barstool.com', companyName: 'Bechtelar Group', stars: 2, downloads: 94, price: 2, id: '13',
+}, {
+  createdAt: '2022-08-25T21:26:45.731Z', name: 'Sylvia Schmitt', description: 'architecto adipisci molestiae', imagePath: 'http://loremflickr.com/640/480/fashion', companyLink: 'https://handsome-coordinator.org', companyName: 'Medhurst, Herzog and Hackett', stars: 5, downloads: 80, price: 71, id: '14',
+}, {
+  createdAt: '2022-08-26T17:33:39.292Z', name: 'Marion Hartmann', description: 'natus id quasi', imagePath: 'http://loremflickr.com/640/480/abstract', companyLink: 'https://ripe-sunshine.org', companyName: 'Wolf - Littel', stars: 67, downloads: 13, price: 84, id: '15',
+}, {
+  createdAt: '2022-08-25T19:44:04.512Z', name: 'Stanley Cormier', description: 'repellat nulla explicabo', imagePath: 'http://loremflickr.com/640/480/people', companyLink: 'http://lean-trove.com', companyName: 'Marquardt - Lakin', stars: 80, downloads: 46, price: 93, id: '16',
+}, {
+  createdAt: '2022-08-26T06:11:52.565Z', name: 'Spencer Quigley', description: 'perspiciatis et quis', imagePath: 'http://loremflickr.com/640/480/food', companyLink: 'https://informal-lipid.name', companyName: 'Hermiston - Braun', stars: 84, downloads: 82, price: 73, id: '17',
+}, {
+  createdAt: '2022-08-25T22:22:49.044Z', name: 'Victoria Hettinger', description: 'rem dolorum harum', imagePath: 'http://loremflickr.com/640/480/nature', companyLink: 'http://rubbery-communion.biz', companyName: 'Koss - Rempel', stars: 36, downloads: 18, price: 14, id: '18',
+}, {
+  createdAt: '2022-08-26T00:57:36.891Z', name: 'Terrence Tillman', description: 'commodi ea porro', imagePath: 'http://loremflickr.com/640/480/sports', companyLink: 'https://revolving-meeting.org', companyName: 'Hirthe - Schultz', stars: 50, downloads: 69, price: 93, id: '19',
+}, {
+  createdAt: '2022-08-26T14:20:14.821Z', name: 'Ms. Johnathan Jacobs', description: 'accusantium laborum aut', imagePath: 'http://loremflickr.com/640/480/technics', companyLink: 'https://creamy-abolishment.name', companyName: 'Jones - Heathcote', stars: 89, downloads: 31, price: 83, id: '20',
+}, {
+  createdAt: '2022-08-26T17:29:45.963Z', name: 'Miss Rita Johns', description: 'officia soluta ad', imagePath: 'http://loremflickr.com/640/480/cats', companyLink: 'http://far-off-ascot.net', companyName: 'Sanford and Sons', stars: 15, downloads: 45, price: 42, id: '21',
+}]
 
 const Search = () => {
   const [search, setSearch] = React.useState('')
 
   return (
     <div className='dark:bg-gray-900 py-10'>
-      <div className='mx-auto max-w-6xl'>
+      <div className='mx-auto max-w-7xl'>
         <form className='flex rounded-md shadow-sm !max-w-[360px] !w-full' onSubmit={() => {}}>
           <Input
             type='text'
@@ -45,9 +76,9 @@ const Search = () => {
             <SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
           </button>
         </form>
-        <div className='flex items-center justify-between mt-5'>
+        <div className='flex items-center justify-between mt-8'>
           <div>
-            <span className='text-gray-700 dark:text-gray-300 text-lg font-medium'>1538 Results</span>
+            <span className='text-gray-500 dark:text-gray-300 text-lg font-medium'>1538 Results</span>
           </div>
           <div className='flex items-center justify-between'>
             <div className='mr-5'>
@@ -71,6 +102,11 @@ const Search = () => {
               />
             </div>
           </div>
+        </div>
+        <div className='flex justify-between items-center flex-wrap mt-4'>
+          {_map(testData, ((item) => (
+            <ExtensionsCard key={item.id} name={item.name} stars={item.stars} downloads={item.downloads} price={item.price} companyLink={item.companyLink} companyName={item.companyName} imagePath={item.imagePath} />
+          )))}
         </div>
       </div>
     </div>
