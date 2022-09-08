a3e528c24f73b15105522944118dc886f1d4db2f
kruzhambus <maks.mrug.ru@gmail.com>
2022-09-08 16:25:40 +0300

    alerts fixed

--- src/redux/reducers/alerts.js
+++ src/redux/reducers/alerts.js
@@ -9,8 +9,8 @@ const initialState = {
 const alertsReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case types.ACCOUNT_UPDATED:
-    case types.DELETE_PROJECT_SUCCESS:
-    case types.CREATE_NEW_PROJECT_SUCCESS:
+    case types.DELETE_EXTENSION_SUCCESS:
+    case types.CREATE_NEW_EXTENSION_SUCCESS:
       return { ...state, message: payload.message, type: payload.type }
 
     case types.CLEAR_ALERTS:
