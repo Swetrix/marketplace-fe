c5a3575c7db22c2cf62c6b500a394be0fb220381
Blue Mouse <durexp@protonmail.ch>
2022-10-27 23:09:42 +0300

    fix

--- src/redux/sagas/ui/load_publish_extensions.js
+++ src/redux/sagas/ui/load_publish_extensions.js
@@ -7,7 +7,7 @@ import UIActions from 'redux/actions/ui'
 
 import { ENTRIES_PER_PAGE_DASHBOARD } from 'redux/constants'
 import {
-  getPublishExtensions, getOverallStats, getLiveVisitors,
+  getPublishExtensions, getLiveVisitors,
 } from '../../../api'
 
 const debug = Debug('swetrix:rx:s:load-projects')
@@ -20,31 +20,16 @@ export default function* loadPublishExtensions({ payload: { take = ENTRIES_PER_P
       // eslint-disable-next-line prefer-const
       extensions, count,
     } = yield call(getPublishExtensions, take, skip)
-    const projectsWithShared = _map(extensions, (project) => {
+    extensions = _map(extensions, (extension) => {
       return {
-        ...project,
+        ...extension,
         project: {
-          ...project.project,
+          ...extension.project,
           shared: true,
         },
       }
     })
     const pids = _map(projectsWithShared, ({ project }) => project.id)
-    let overall
-
-    try {
-      overall = yield call(getOverallStats, pids)
-    } catch (e) {
-      debug('failed to overall stats: %s', e)
-    }
-
-    extensions = _map(projectsWithShared, res => ({
-      ...res,
-      project: {
-        ...res.project,
-        overall: overall?.[res.project.id],
-      },
-    }))
 
     yield put(UIActions.setExtensions(extensions, true))
     yield put(UIActions.setTotal(count, true))
