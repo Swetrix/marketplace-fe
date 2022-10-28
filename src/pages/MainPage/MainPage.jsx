d2a3c270eaccdfb8be0eecffb32a7405a49e8b3d
Blue Mouse <durexp@protonmail.ch>
2022-10-28 12:51:32 +0300

    some improvements

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Input from 'ui/Input'
 import { SearchIcon } from '@heroicons/react/outline'
+import { useTranslation } from 'react-i18next'
 import Button from 'ui/Button'
 import _isEmpty from 'lodash/isEmpty'
 import Glider from 'react-glider'
@@ -9,8 +10,10 @@ import { useHistory } from 'react-router-dom'
 import { sortByConstans } from 'redux/constants'
 import ExtensionsCard from 'components/ExtensionsCard'
 import _map from 'lodash/map'
+import Title from 'components/Title'
 
 const MainPage = ({ extensions }) => {
+  const { t } = useTranslation()
   const [search, setSearch] = useState('')
   const history = useHistory()
 
@@ -26,69 +29,71 @@ const MainPage = ({ extensions }) => {
   }
 
   return (
-    <div className='dark:bg-gray-900'>
-      <div>
-        <h1 className='text-center pt-16 font-extrabold text-3xl dark:text-white text-gray-700'>
-          Extensions for Swetrix Analytics
-        </h1>
-        <form
-          className='mt-5 mx-auto flex rounded-md shadow-sm !max-w-[360px] !w-full'
-          onSubmit={searchSubmit}
-        >
-          <Input
-            type='text'
-            label=''
-            value={search}
-            placeholder='Search extensions for Swetrix Analytics'
-            classNameInpit='!rounded-none !rounded-l-md'
-            className='!w-full !max-w-full'
-            onChange={(e) => setSearch(e.target.value)}
-            disabled={false}
-            error={false}
-          />
-          <button
-            type='button'
-            className='-ml-px mb-2 mt-1 relative inline-flex items-center space-x-2 px-4 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 dark:bg-gray-800 dark:border-0 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
+    <Title title={t('titles.main')}>
+      <div className='dark:bg-gray-900'>
+        <div>
+          <h1 className='text-center pt-16 font-extrabold text-3xl dark:text-white text-gray-700'>
+            Extensions for Swetrix Analytics
+          </h1>
+          <form
+            className='mt-5 mx-auto flex rounded-md shadow-sm !max-w-[360px] !w-full'
+            onSubmit={searchSubmit}
           >
-            <SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-          </button>
-        </form>
-      </div>
-      <section>
-        <div className='max-w-[1400px] mx-auto py-10 px-4 sm:px-6 lg:px-8'>
-          <div className='flex items-center justify-between'>
-            <h2 className='text-2xl font-bold tracking-tight text-gray-800 dark:text-white'>
-              Featured
-            </h2>
-            <Button onClick={() => {}} text='See more' primary regular />
-          </div>
-          <div className='mt-6 relative p-5'>
-            <Glider
-              hasArrows
-              slidesToScroll={6}
-              scrollLock
-              resizeLock
-              exactWidth
-              itemWidth={210}
+            <Input
+              type='text'
+              label=''
+              value={search}
+              placeholder='Search extensions for Swetrix Analytics'
+              classNameInpit='!rounded-none !rounded-l-md'
+              className='!w-full !max-w-full'
+              onChange={(e) => setSearch(e.target.value)}
+              disabled={false}
+              error={false}
+            />
+            <button
+              type='button'
+              className='-ml-px mb-2 mt-1 relative inline-flex items-center space-x-2 px-4 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 dark:bg-gray-800 dark:border-0 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
             >
-              {_map(extensions, (extension) => (
-                <ExtensionsCard
-                  key={extension.id}
-                  id={extension.id}
-                  name={extension.name}
-                  stars={3}
-                  downloads={extension.usersQuantity}
-                  imagePath={extension.mainImage}
-                  // price={extension.price}
-                  // companyLink='https://simpson.com'
-                  companyName={extension.owner?.nickname || 'Unknown'}
-                />
-              ))}
-            </Glider>
-          </div>
+              <SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+            </button>
+          </form>
         </div>
-      </section>
-    </div>
+        <section>
+          <div className='max-w-[1400px] mx-auto py-10 px-4 sm:px-6 lg:px-8'>
+            <div className='flex items-center justify-between'>
+              <h2 className='text-2xl font-bold tracking-tight text-gray-800 dark:text-white'>
+                Featured
+              </h2>
+              <Button onClick={() => { }} text='See more' primary regular />
+            </div>
+            <div className='mt-6 relative p-5'>
+              <Glider
+                hasArrows
+                slidesToScroll={6}
+                scrollLock
+                resizeLock
+                exactWidth
+                itemWidth={210}
+              >
+                {_map(extensions, (extension) => (
+                  <ExtensionsCard
+                    key={extension.id}
+                    id={extension.id}
+                    name={extension.name}
+                    stars={3}
+                    downloads={extension.usersQuantity}
+                    imagePath={extension.mainImage}
+                    // price={extension.price}
+                    // companyLink='https://simpson.com'
+                    companyName={extension.owner?.nickname || 'Unknown'}
+                  />
+                ))}
+              </Glider>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Title>
   )
 }
 
--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -13,7 +13,8 @@ import { sortByConstans } from 'redux/constants'
 import _values from 'lodash/values'
 import _map from 'lodash/map'
 import _ceil from 'lodash/ceil'
-import _isEmputy from 'lodash/isEmpty'
+import _isEmpty from 'lodash/isEmpty'
+import Title from 'components/Title'
 
 import { getExtensionsSearch } from 'api'
 
@@ -58,76 +59,78 @@ const Search = ({
   }, [search, filterCategory, filterSortBy, offset, limit])
 
   useEffect(() => {
-    history.push(`/search?term=${search}&category=${_isEmputy(filterCategory) ? '' : filterCategory}&sortBy=${filterSortBy}`)
+    history.push(`/search?term=${search}&category=${_isEmpty(filterCategory) ? '' : filterCategory}&sortBy=${filterSortBy}`)
   }, [search, filterCategory, filterSortBy, history])
 
   return (
-    <div className='dark:bg-gray-900 py-10 px-2 sm:px-10'>
-      <div className='mx-auto max-w-7xl'>
-        <form className='flex rounded-md shadow-sm !max-w-[360px] !w-full' onSubmit={() => {}}>
-          <Input
-            type='text'
-            label=''
-            value={search}
-            placeholder='Search extensions for Swetrix Analytics'
-            classNameInpit='!rounded-none !rounded-l-md'
-            className='!w-full !max-w-full'
-            onChange={(e) => setSearch(e.target.value)}
-            disabled={false}
-            error={false}
-          />
-          <button
-            type='submit'
-            className='-ml-px mb-2 mt-1 relative inline-flex items-center space-x-2 px-4 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 dark:bg-gray-800 dark:border-0 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
-          >
-            <SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-          </button>
-        </form>
-        <div className='flex items-center justify-between mt-8'>
-          <div>
-            <span className='text-gray-500 dark:text-gray-300 text-lg font-medium'>1538 Results</span>
-          </div>
-          <div className='flex items-center justify-between'>
-            <div className='mr-5'>
-              <span className='mr-3 dark:text-gray-200'>Showing:</span>
-              <Dropdown
-                items={_values(category)}
-                title={filterCategory}
-                buttonClassName='flex items-center w-full rounded-md border border-gray-300 shadow-sm px-1 md:px-2 py-1 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
-                selectItemClassName='text-gray-700 block px-2 py-1 text-base cursor-pointer hover:bg-gray-200 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
-                onSelect={setFilterCategory}
-              />
-            </div>
+    <Title title='Search'>
+      <div className='dark:bg-gray-900 py-10 px-2 sm:px-10 min-h-page'>
+        <div className='mx-auto max-w-7xl'>
+          <form className='flex rounded-md shadow-sm !max-w-[360px] !w-full' onSubmit={() => { }}>
+            <Input
+              type='text'
+              label=''
+              value={search}
+              placeholder='Search extensions for Swetrix Analytics'
+              classNameInpit='!rounded-none !rounded-l-md'
+              className='!w-full !max-w-full'
+              onChange={(e) => setSearch(e.target.value)}
+              disabled={false}
+              error={false}
+            />
+            <button
+              type='submit'
+              className='-ml-px mb-2 mt-1 relative inline-flex items-center space-x-2 px-4 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 dark:bg-gray-800 dark:border-0 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
+            >
+              <SearchIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
+            </button>
+          </form>
+          <div className='flex items-center justify-between mt-8'>
             <div>
-              <span className='mr-3  dark:text-gray-200'>Sort By:</span>
-              <Dropdown
-                items={_values(sortByConstans)}
-                title={filterSortBy}
-                buttonClassName='flex items-center w-full rounded-md border border-gray-300 shadow-sm px-1 md:px-2 py-1 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
-                selectItemClassName='text-gray-700 block px-2 py-1 text-base cursor-pointer hover:bg-gray-200 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
-                onSelect={setFilterSortBy}
-              />
+              <span className='text-gray-500 dark:text-gray-300 text-lg font-medium'>1538 Results</span>
+            </div>
+            <div className='flex items-center justify-between'>
+              <div className='mr-5'>
+                <span className='mr-3 dark:text-gray-200'>Showing:</span>
+                <Dropdown
+                  items={_values(category)}
+                  title={filterCategory}
+                  buttonClassName='flex items-center w-full rounded-md border border-gray-300 shadow-sm px-1 md:px-2 py-1 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
+                  selectItemClassName='text-gray-700 block px-2 py-1 text-base cursor-pointer hover:bg-gray-200 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
+                  onSelect={setFilterCategory}
+                />
+              </div>
+              <div>
+                <span className='mr-3  dark:text-gray-200'>Sort By:</span>
+                <Dropdown
+                  items={_values(sortByConstans)}
+                  title={filterSortBy}
+                  buttonClassName='flex items-center w-full rounded-md border border-gray-300 shadow-sm px-1 md:px-2 py-1 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
+                  selectItemClassName='text-gray-700 block px-2 py-1 text-base cursor-pointer hover:bg-gray-200 dark:text-gray-50 dark:border-gray-800 dark:bg-gray-700 dark:hover:bg-gray-600'
+                  onSelect={setFilterSortBy}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className='flex items-center flex-wrap justify-center md:justify-between mt-4 gap-1'>
-          {loading
-            ? (
-              <div className='mx-auto'>
-                <Loader />
-              </div>
-            )
-            : _map(extensions, ((item) => (
-              <ExtensionsCard key={item.id} name={item.name} stars={4} downloads={1000} price={item.price} companyLink='swetrix' companyName='companyName' mainImage={item.mainImage} />
-            )))}
-        </div>
-        {pageAmount > 1 && (
-          <div className='mt-2'>
-            <Pagination page={page} setPage={setPage} pageAmount={pageAmount} total={total} />
+          <div className='flex items-center flex-wrap justify-center md:justify-between mt-4 gap-1'>
+            {loading
+              ? (
+                <div className='mx-auto'>
+                  <Loader />
+                </div>
+              )
+              : _map(extensions, ((item) => (
+                <ExtensionsCard key={item.id} name={item.name} stars={4} downloads={1000} price={item.price} companyLink='swetrix' companyName='companyName' mainImage={item.mainImage} />
+              )))}
           </div>
-        )}
+          {pageAmount > 1 && (
+            <div className='mt-2'>
+              <Pagination page={page} setPage={setPage} pageAmount={pageAmount} total={total} />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </Title>
   )
 }
 
