b860e7076a248a75acde8334693364738d10d98b
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-07-01 16:45:16 +0300

    create commentMenu

--- src/pages/Extension/View/ExtensionPage/ExtensionPage.jsx
+++ src/pages/Extension/View/ExtensionPage/ExtensionPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { Fragment, useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 // import StarsRaiting from 'ui/StarsRaiting'
 import Glider from 'react-glider'
@@ -13,6 +13,78 @@ import Title from 'components/Title'
 import { extensionStatuses } from 'redux/constants'
 import StarsRaiting from 'ui/StarsRaiting'
 import { ExtensionCommentList } from 'data/ExtensionCommentList'
+import Dropdown from 'ui/Dropdown'
+import cx from 'clsx'
+import { Menu, Transition } from '@headlessui/react'
+
+
+
+const CommentMenu = () => {
+	return (
+		<Menu as='div' className='relative'>
+		<div>
+			<Menu.Button className='p-1 flex justify-center items-center font-semibold leading-6 text-base text-slate-800 hover:text-slate-700 dark:text-slate-200 dark:hover:text-white'>
+			<svg className='w-5 h-5' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20'
+										xmlns='http://www.w3.org/2000/svg'>
+										<path
+												d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z'>
+										</path>
+								</svg>
+			</Menu.Button>
+		</div>
+		<Transition
+			as={Fragment}
+			enter='transition ease-out duration-100'
+			enterFrom='transform opacity-0 scale-95'
+			enterTo='transform opacity-100 scale-100'
+			leave='transition ease-in duration-75'
+			leaveFrom='transform opacity-100 scale-100'
+			leaveTo='transform opacity-0 scale-95'
+		>
+			<Menu.Items className='absolute right-0 z-30 mt-2 w-60 min-w-max origin-top-right rounded-md bg-white dark:bg-slate-900 py-1 shadow-lg ring-1 ring-slate-200 dark:ring-slate-800 focus:outline-none'>
+					<Menu.Item>
+					{({ active }) => (
+						<div
+							className={cx('cursor-pointer block px-4 py-2 text-sm text-gray-700 dark:text-gray-50', {
+								'bg-gray-100 dark:bg-slate-800': active,
+							})}
+						onClick={() => console.log('Edit')}
+						>
+							Edit
+						</div>
+					)}
+					</Menu.Item>
+
+				<Menu.Item>
+					{({ active }) => (
+						<div
+						className={cx('cursor-pointer block px-4 py-2 text-sm text-gray-700 dark:text-gray-50', {
+							'bg-gray-100 dark:bg-slate-800': active,
+						})}
+						onClick={() => console.log('Remove')}
+						>
+							Remove
+						</div>
+					)}
+				</Menu.Item>
+
+				<Menu.Item>
+					{({ active }) => (
+						<div
+							className={cx('cursor-pointer px-4 py-2 text-sm text-gray-700 dark:text-gray-50', {
+								'bg-gray-100 dark:bg-slate-800': active,
+							})}
+							onClick={() => console.log('Report')}
+						>
+							Report
+						</div>
+					)}
+				</Menu.Item>
+			</Menu.Items>
+		</Transition>
+	</Menu>
+	)
+}
 
 const ExtensionPage = ({ extensions, showError, setExtensions, installExtensions, authenticated, publishExtensions, user }) => {
   const { id } = useParams()
@@ -206,154 +278,89 @@ const ExtensionPage = ({ extensions, showError, setExtensions, installExtensions
               )}
             </div>
           </div>
+					<section className='bg-white dark:bg-gray-900 py-8 lg:py-16'>
+						<div className='max-w-2xl mx-auto px-4'>
+							<div className='flex flex-col justify-start items-start mb-6'>
+								<h2 className='text-lg lg:text-2xl font-bold text-gray-900 dark:text-white'>Discussion (20)</h2>
+								<div className='flex flex-row items-center gap-1 mt-2'>
+									<StarsRaiting stars='3.5' />
+								</div>
+						</div>
+						<form className='mb-6'>
+							<div className='py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
+							<label htmlFor='comment' className='sr-only'>Your comment</label>
+							<textarea id='comment' rows='6'
+									className='px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800'
+									placeholder='Write a comment...' required></textarea>
+							</div>
+							<div className='flex justify-end'>
+								<Button
+									type='submit'
+									primary
+									className='inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800'
+								>
+									Submit
+								</Button>
+								</div>
+						</form>
+							{
+								_map(comments, (item) => (
+									<div key={item.id}>
+										<article className='p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900'>
+											<footer className='flex justify-between items-center mb-2'>
+											<div className='flex items-center'>
+										<p className='inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white'><img
+														className='mr-2 w-6 h-6 rounded-full'
+														src={item.icon}
+														alt='Michael Gough'/>{item.name}</p>
+										<p className='text-sm text-gray-600 dark:text-gray-400'><time  dateTime='2022-02-08'
+														title='February 8th, 2022'>{item.data}</time></p>
+											</div>
+										<div>
+									<CommentMenu/>
+								</div>
+											</footer>
+											<p className='text-gray-500 dark:text-gray-400'>{item.description}</p>
+											<div className='flex items-center mt-4 space-x-4'>
+								<button type='button'
+										className='flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400'>
+										<svg aria-hidden='true' className='mr-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'></path></svg>
+										Reply
+								</button>
+											</div>
+										</article>
+										
+										{
+											item.subComment && _map(item.subComment, (subItem) => (
+												<article className='p-6 mb-6 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900'>
+													<footer className='flex justify-between items-center mb-2'>
+													<div className='flex items-center'>
+														<p className='inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white'><img
+															className='mr-2 w-6 h-6 rounded-full'
+															src={subItem.icon}
+															alt='Jese Leos'/>{subItem.name}</p>
+														<p className='text-sm text-gray-600 dark:text-gray-400'><time  dateTime='2022-02-12'
+															title='February 12th, 2022'>{subItem.data}</time></p>
+														</div>
+														<CommentMenu/>
+														</footer>
+														<p className='text-gray-500 dark:text-gray-400'>{subItem.description}</p>
+														<div className='flex items-center mt-4 space-x-4'>
+														<button type='button'
+														className='flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400'>
+															<svg aria-hidden='true' className='mr-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'></path></svg>
+															Reply
+														</button>
+													</div>
+												</article>
+											))
+										}
+									</div>
+								))
+							}
+						</div>
+					</section>
         </div>
-
-				<section className='bg-white dark:bg-gray-900 py-8 lg:py-16'>
-        <div className='max-w-2xl mx-auto px-4'>
-            <div className='flex flex-col justify-start items-start mb-6'>
-            <h2 className='text-lg lg:text-2xl font-bold text-gray-900 dark:text-white'>Discussion (20)</h2>
-            
-            <div className='flex flex-row items-center gap-1 mt-2'>
-              <StarsRaiting stars='3.5' />
-            </div>
-          
-          </div>
-          <form className='mb-6'>
-          
-            <div className='py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
-            <label htmlFor='comment' className='sr-only'>Your comment</label>
-
-            <textarea id='comment' rows='6'
-                className='px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800'
-                placeholder='Write a comment...' required></textarea>
-            </div>
-
-            <div className='flex justify-end'>
-            <Button
-              type='submit'
-              primary
-              className='inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800'
-            >
-              Submit
-            </Button>
-            </div>
-          </form>
-
-          {
-            _map(comments, (item) => (
-              <div key={item.id}>
-                <article className='p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900'>
-                  <footer className='flex justify-between items-center mb-2'>
-                  <div className='flex items-center'>
-                <p className='inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white'><img
-                        className='mr-2 w-6 h-6 rounded-full'
-                        src={item.icon}
-                        alt='Michael Gough'/>{item.name}</p>
-                <p className='text-sm text-gray-600 dark:text-gray-400'><time  dateTime='2022-02-08'
-                        title='February 8th, 2022'>{item.data}</time></p>
-                  </div>
-                  <button id='dropdownComment1Button' data-dropdown-toggle='dropdownComment1'
-                className='inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-                type='button'>
-                <svg className='w-5 h-5' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                        d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z'>
-                    </path>
-                </svg>
-                <span className='sr-only'>Comment settings</span>
-                  </button>
-            
-                  <div id='dropdownComment1'
-                className='hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'>
-                <ul className='py-1 text-sm text-gray-700 dark:text-gray-200'
-                    aria-labelledby='dropdownMenuIconHorizontalButton'>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Edit</a>
-                    </li>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Remove</a>
-                    </li>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Report</a>
-                    </li>
-                </ul>
-                  </div>
-                  </footer>
-                  <p className='text-gray-500 dark:text-gray-400'>{item.description}</p>
-                  <div className='flex items-center mt-4 space-x-4'>
-            <button type='button'
-                className='flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400'>
-                <svg aria-hidden='true' className='mr-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'></path></svg>
-                Reply
-            </button>
-                  </div>
-                </article>
-
-                
-                {
-                  item.subComment && _map(item.subComment, (subItem) => (
-                    <article className='p-6 mb-6 ml-6 lg:ml-12 text-base bg-white rounded-lg dark:bg-gray-900'>
-                      <footer className='flex justify-between items-center mb-2'>
-                      <div className='flex items-center'>
-                      <p className='inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white'><img
-                        className='mr-2 w-6 h-6 rounded-full'
-                        src={subItem.icon}
-                        alt='Jese Leos'/>{subItem.name}</p>
-                      <p className='text-sm text-gray-600 dark:text-gray-400'><time  dateTime='2022-02-12'
-                        title='February 12th, 2022'>{subItem.data}</time></p>
-                      </div>
-                      <button id='dropdownComment2Button' data-dropdown-toggle='dropdownComment2'
-                className='inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-                type='button'>
-                <svg className='w-5 h-5' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20'
-                    xmlns='http://www.w3.org/2000/svg'>
-                    <path
-                        d='M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z'>
-                    </path>
-                </svg>
-                <span className='sr-only'>Comment settings</span>
-                      </button>
-            
-                      <div id='dropdownComment2'
-                className='hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600'>
-                <ul className='py-1 text-sm text-gray-700 dark:text-gray-200'
-                    aria-labelledby='dropdownMenuIconHorizontalButton'>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Edit</a>
-                    </li>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Remove</a>
-                    </li>
-                    <li>
-                        <a href='#'
-                            className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>Report</a>
-                    </li>
-                </ul>
-                      </div>
-                      </footer>
-                      <p className='text-gray-500 dark:text-gray-400'>{subItem.description}</p>
-                      <div className='flex items-center mt-4 space-x-4'>
-            <button type='button'
-                className='flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400'>
-                <svg aria-hidden='true' className='mr-1 w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'></path></svg>
-                Reply
-            </button>
-                      </div>
-                    </article>
-                  ))
-                }
-              </div>
-            ))
-          }
-   
-        </div>
-</section>
       </Title>
     </>
   )
