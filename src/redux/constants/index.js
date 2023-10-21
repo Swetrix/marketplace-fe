af50cc5a971b60c74e30674de7d283b475f0b1e4
Blue Mouse <durexp@protonmail.ch>
2023-10-21 04:40:09 +0100

    set ENTRIES_PER_PAGE_COMMENTS to 10

--- src/redux/constants/index.js
+++ src/redux/constants/index.js
@@ -65,8 +65,7 @@ export const languageFlag = {
 
 export const ENTRIES_PER_PAGE_DASHBOARD = 10
 export const ENTRIES_PER_PAGE_EXTENSIONS = 30
-export const ENTRIES_PER_PAGE_COMMENTS = 2
-
+export const ENTRIES_PER_PAGE_COMMENTS = 10
 
 export const tabForInstallExtension = 'installed'
 export const tabForPublishExtensions = 'published'
